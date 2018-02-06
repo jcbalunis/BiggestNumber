a1bc495defb6870801181e62712a667be9926384
jcbalunis <jbalunis@mail.csuchico.edu>
2018-02-05 17:21:59 -0800

    Asking for Whole Numbers

--- biggestnumber.cpp
+++ biggestnumber.cpp
@@ -5,10 +5,14 @@ using namespace std; //so we don't need the prefix std::
 
 int main()
 {
-  
-  cout<<"Please enter a whole number:\n";
-  
-  cout<<"Please enter another whole number:\n";
+int number1;
+int number2;
+
+
+  std::cout<<"Please enter a whole number:\n";
+  std::cin>> number1;
+  std::cout<<"Please enter another whole number:\n";
+  std::cin>> number2;
 
   cout<<"Of those two numbers, the biggest is: ";
   cout<<endl<<"Thank you for playing.\n";
