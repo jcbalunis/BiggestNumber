b5b8f92b47c6dd3c734a312fd0178e911f6e3322
jcbalunis <jbalunis@mail.csuchico.edu>
2018-02-05 17:51:53 -0800

    Decides what the biggest number is

--- biggestnumber.cpp
+++ biggestnumber.cpp
@@ -7,15 +7,27 @@ int main()
 {
 int number1;
 int number2;
-
+int biggest;
 
   std::cout<<"Please enter a whole number:\n";
   std::cin>> number1;
   std::cout<<"Please enter another whole number:\n";
   std::cin>> number2;
 
-  cout<<"Of those two numbers, the biggest is: ";
-  cout<<endl<<"Thank you for playing.\n";
+
+
+    if (number1 > number2)
+    { 
+    biggest = number1;
+    }
+    else
+    {
+    biggest = number2;
+    }
+
+  std::cout<<"Of those two numbers, the biggest is: ";
+  std::cout<< biggest;
+  std::cout<<endl<<"Thank you for playing.\n";
 
   return 0;
 }
